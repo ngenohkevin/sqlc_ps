095566ac14fe5f3c43a2c810b5ea89073c973131
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-03 12:33:19 +0300

    connecting to server

--- main.go
+++ main.go
@@ -1,34 +1,14 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	_ "github.com/lib/pq"
-	db2 "github.com/ngenohkevin/sqlc_ps/db/sqlc"
-	"log"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:dylstar20@localhost:5432/sqlc_db?sslmode=disable"
 )
 
 func main() {
-	conn, err := sql.Open("postgres", "postgresql://postgres:dylstar20@localhost:5432/sqlc_db?sslmode=disable")
-	if err != nil {
-		log.Fatal("Unable to connect to the database", err)
-	}
-	db := db2.New(conn)
-
-	user, err := db.CreateUser(context.Background(), db2.CreateUserParams{
-		Firstname: "Jay",
-		Lastname:  "Maina",
-	})
-
-	todo, err := db.CreateTodo(context.Background(), db2.CreateTodoParams{
-		UsersID: user.ID,
-		Task:    "Bathing",
-		Done:    true,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println(user, todo)
 
 }
