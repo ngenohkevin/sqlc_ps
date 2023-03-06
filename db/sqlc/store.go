1705d341e1463e23345232690eb566e4d29b63d2
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-06 11:35:31 +0300

    generated mockdb

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,14 +1,21 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
-type Store struct {
+type Store interface {
+	Querier
+	//TransferTX(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
